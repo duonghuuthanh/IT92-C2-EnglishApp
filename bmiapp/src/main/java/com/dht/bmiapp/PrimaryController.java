2c70fd7779cfac0a74ced78f78cb819d666eb1d9
dhthanhqa@gmail.com <dhthanhqa@gmail.com>
2022-03-02 10:36:49 +0700

    update bmiapp c2

--- bmiapp/src/main/java/com/dht/bmiapp/PrimaryController.java
+++ bmiapp/src/main/java/com/dht/bmiapp/PrimaryController.java
@@ -1,5 +1,29 @@
 package com.dht.bmiapp;
 
-public class PrimaryController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 
+public class PrimaryController {
+    @FXML private TextField txtHeight;
+    @FXML private TextField txtWeight;
+    @FXML private Label lblResult;
+    
+    public void bmiHandler(ActionEvent event) {
+        double height = Double.parseDouble(this.txtHeight.getText());
+        double weight = Double.parseDouble(this.txtWeight.getText());
+        double bmi = weight / Math.pow(height, 2);
+        
+        this.lblResult.setTextFill(Color.DARKBLUE);
+        if (bmi < 18.5)
+            lblResult.setText("Gầy");
+        else if (bmi < 25.5)
+            lblResult.setText("Bình thường");
+        else {
+            this.lblResult.setTextFill(Color.RED);
+            lblResult.setText("Thừa cân");
+        }
+    }
 }
